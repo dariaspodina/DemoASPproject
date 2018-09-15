e1698d54f3dd23933ca1d77d8dd31d0913af1b4e
Daria <dasha.vda.93@gmail.com>
2018-09-15 17:50:08 +0300

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.My = hour < 12 ? "Goog morning" : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponce guestResponce)
+        {
+            //проверка валидации данных формы
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponce);
+            }
+            else
+            {
+                //ошибка валидации
+                return View();
+            }
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponce.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponce
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email address")]
+        [RegularExpression((@".+\@.+\..+"), ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [RegularExpression((@"((\+38|8|\+3|\+ )[ ]?)?([(]?\d{3}[)]?[\- ]?)?(\d[ -]?){6,14}"), ErrorMessage = "Please enter a valid phone number")]
+        public string Phone { get; set; }
+
+        //bool? - true, false, null. Если значение не выбрано то значение = null, что помогает провести валидацию с помощью атрибута 
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
